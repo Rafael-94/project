e2a6c993c5791d6889f01604e8fb4f60f37f6e40
Rafael Zarate <rrzarate4@misena.edu.co>
2024-04-10 20:29:06 -0500

    Arreglos

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tareas',
-    'templates',
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
